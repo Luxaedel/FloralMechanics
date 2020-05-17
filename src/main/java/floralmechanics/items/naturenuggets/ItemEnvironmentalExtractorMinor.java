5b8915b55d0b551f97e3271ab6278a272f9f7722
Standaard <yannick.lucassen@gmail.com>
2020-05-17 15:13:46 +0200

    Updated parameters

--- src/main/java/floralmechanics/items/naturenuggets/ItemEnvironmentalExtractorMinor.java
+++ src/main/java/floralmechanics/items/naturenuggets/ItemEnvironmentalExtractorMinor.java
@@ -1,7 +1,7 @@
 package floralmechanics.items.naturenuggets;
 
 public class ItemEnvironmentalExtractorMinor extends ItemEnvironmentalExtractorBase {
-	public ItemEnvironmentalExtractorMinor(String name) {
-		super(name);
+	public ItemEnvironmentalExtractorMinor(String name, int dropOddsPerAction, int blockBreakOdds, int baseExtractDuration) {
+		super(name, dropOddsPerAction, blockBreakOdds, baseExtractDuration);
 	}
 }
