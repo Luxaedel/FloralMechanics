e31bf1d5ac2e4a157ed394de4aa00da6bce7a41b
Standaard <yannick.lucassen@gmail.com>
2020-05-17 15:16:49 +0200

    - Added extra parameters - Enabled more than 100% items being received from a block - Enabled fortune working for blocks that support fortune mining - Enabled efficiency to decrease 'extraction time' with a baseline of 10 ticks per action - Small optimizations - Stop checking when item has been finished using, check in update loop when action completes instead.

--- src/main/java/floralmechanics/items/naturenuggets/ItemEnvironmentalExtractorBase.java
+++ src/main/java/floralmechanics/items/naturenuggets/ItemEnvironmentalExtractorBase.java
@@ -1,13 +1,17 @@
 package floralmechanics.items.naturenuggets;
 
 import java.util.List;
+import java.util.Random;
 
 import floralmechanics.items.ItemBase;
 import net.minecraft.block.Block;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Enchantments;
 import net.minecraft.item.EnumAction;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
@@ -20,8 +24,16 @@ import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 public class ItemEnvironmentalExtractorBase extends ItemBase {
-	public ItemEnvironmentalExtractorBase(String name) {
+	protected final Random RANDOM = new Random();
+	protected final int efficiencyTickDecreasePerLevel = 5;
+	protected int dropOddsPerAction = 0, blockBreakOdds = 0, guaranteedDrops = 0, baseExtractDuration = 0;
+	
+	public ItemEnvironmentalExtractorBase(String name, int dropOddsPerAction, int blockBreakOdds, int baseExtractDuration) {
 		super(name);
+		this.guaranteedDrops = Math.floorDiv(dropOddsPerAction, 100);
+		this.dropOddsPerAction = dropOddsPerAction - (this.guaranteedDrops * 100);
+		this.blockBreakOdds = blockBreakOdds;
+		this.baseExtractDuration = baseExtractDuration;
 	}
 	
 	@Override
@@ -31,11 +43,18 @@ public class ItemEnvironmentalExtractorBase extends ItemBase {
 	
 	@Override
 	public int getMaxItemUseDuration(ItemStack stack) {
-		return 60;
+		return Math.max(10, this.baseExtractDuration - (EnchantmentHelper.getEnchantmentLevel(Enchantments.EFFICIENCY, stack) * this.efficiencyTickDecreasePerLevel));
 	}
 	
 	public ItemStack onItemUseFinish(ItemStack stack, World worldIn, EntityPlayer playerIn) {
 		return stack;
+	}
+	
+	@Override
+	public boolean canApplyAtEnchantingTable(ItemStack stack, Enchantment enchantment) {
+		if (enchantment == Enchantments.FORTUNE) return true;
+		else if (enchantment == Enchantments.EFFICIENCY) return true;
+		return super.canApplyAtEnchantingTable(stack, enchantment);
 	}
 	
 	@Override
@@ -45,13 +64,11 @@ public class ItemEnvironmentalExtractorBase extends ItemBase {
 	}
 	
 	@Override
-	public void onPlayerStoppedUsing(ItemStack stack, World worldIn, EntityLivingBase entity, int timeLeft) {
-		//TODO: Possibly, instead of targeting a specific block, select X blocks in Y radius around the player
-		//TODO: and harvest those. With right click extraction like this it might as well be a bog-standard tool
-		
-		if (entity instanceof EntityPlayer) {
+	public void onUsingTick(ItemStack stack, EntityLivingBase entity, int count) {
+		if (count <= 1 && entity instanceof EntityPlayer) {
 			EntityPlayer player = (EntityPlayer)entity;
-			if (!worldIn.isRemote && timeLeft <= getMaxItemUseDuration(stack) - 25) {
+			World worldIn = player.world;
+			if (!worldIn.isRemote) {
 				Vec3d vec3d = player.getPositionVector().add(0,  player.eyeHeight, 0);
 				Vec3d vec3d1 = player.getLookVec();
 				//Vec3d vec3d2 = vec3d.scale(5);// vec3d1 * 5;
@@ -62,17 +79,50 @@ public class ItemEnvironmentalExtractorBase extends ItemBase {
 					Block block = worldIn.getBlockState(pos).getBlock();
 					if (!block.hasTileEntity(worldIn.getBlockState(pos))) {
 						NonNullList<ItemStack> drops = NonNullList.<ItemStack>create();
-						block.getDrops(drops, worldIn, pos, worldIn.getBlockState(pos), 0);
+						block.getDrops(drops, worldIn, pos, worldIn.getBlockState(pos), EnchantmentHelper.getEnchantmentLevel(Enchantments.FORTUNE, stack));
 						for (ItemStack dropStack : drops) {
+							int total = this.RANDOM.nextInt(100) < this.dropOddsPerAction ? this.guaranteedDrops + 1 : this.guaranteedDrops;
+							dropStack.setCount(total);
 							if (!player.addItemStackToInventory(dropStack)) {
 								worldIn.spawnEntity(new EntityItem(worldIn, pos.getX(), pos.getY(), pos.getZ(), dropStack));
 							}
 						}
-						worldIn.setBlockState(pos, Blocks.AIR.getDefaultState(), worldIn.isRemote ? 11 : 3);
-					
+						if (this.RANDOM.nextInt(100) < this.blockBreakOdds) worldIn.setBlockState(pos, Blocks.AIR.getDefaultState(), worldIn.isRemote ? 11 : 3);
 					}
 				}
 			}
 		}
 	}
+	
+//	@Override
+//	public void onPlayerStoppedUsing(ItemStack stack, World worldIn, EntityLivingBase entity, int timeLeft) {
+//		//TODO: Possibly, instead of targeting a specific block, select X blocks in Y radius around the player
+//		//TODO: and harvest those. With right click extraction like this it might as well be a bog-standard tool
+//		
+//		if (entity instanceof EntityPlayer) {
+//			EntityPlayer player = (EntityPlayer)entity;
+//			if (!worldIn.isRemote && timeLeft <= getMaxItemUseDuration(stack) - 25) {
+//				Vec3d vec3d = player.getPositionVector().add(0,  player.eyeHeight, 0);
+//				Vec3d vec3d1 = player.getLookVec();
+//				//Vec3d vec3d2 = vec3d.scale(5);// vec3d1 * 5;
+//				Vec3d vec3d2 = vec3d.add(vec3d1.x * 5, vec3d1.y * 5, vec3d1.z * 5);
+//				RayTraceResult rayTrace = worldIn.rayTraceBlocks(vec3d, vec3d2, false, false, true);
+//				if (rayTrace != null) {
+//					BlockPos pos = rayTrace.getBlockPos();
+//					Block block = worldIn.getBlockState(pos).getBlock();
+//					if (!block.hasTileEntity(worldIn.getBlockState(pos))) {
+//						NonNullList<ItemStack> drops = NonNullList.<ItemStack>create();
+//						block.getDrops(drops, worldIn, pos, worldIn.getBlockState(pos), 0);
+//						for (ItemStack dropStack : drops) {
+//							if (!player.addItemStackToInventory(dropStack)) {
+//								worldIn.spawnEntity(new EntityItem(worldIn, pos.getX(), pos.getY(), pos.getZ(), dropStack));
+//							}
+//						}
+//						worldIn.setBlockState(pos, Blocks.AIR.getDefaultState(), worldIn.isRemote ? 11 : 3);
+//					
+//					}
+//				}
+//			}
+//		}
+//	}
 }
