aeefbb43d67dd65656d783213b74fbbd4ce1d0a6
Standaard <yannick.lucassen@gmail.com>
2020-05-18 19:52:47 +0200

    - Base for fake player check

---
+++ src/main/java/floralmechanics/util/helpers/PlayerHelper.java
@@ -0,0 +1,17 @@
+package floralmechanics.util.helpers;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+
+public class PlayerHelper {
+	//Courtesy of RWTema
+	public static boolean isPlayerReal(EntityPlayer player) {
+		return player != null &&
+			   player.world != null &&
+			   !player.world.isRemote &&
+			   (player.getClass() == EntityPlayerMP.class ||
+						FMLCommonHandler.instance().getMinecraftServerInstance().getPlayerList().getPlayers().contains(player));
+
+	}
+}
